22d1ce6c3cd9ec6caea9670c4834a98ccb2a2d4c
teocollin1995 <teocollin1995@gmail.com>
2015-07-31 19:53:26 -0400

    still trying to undersand app engine

---
+++ main.py
@@ -0,0 +1,30 @@
+import json
+import logging
+import urllib
+import urllib2
+from google.appengine.api import urlfetch
+from google.appengine.ext import ndb
+from flask import Flask
+
+TOKEN= '50177117:AAGCMNPVi73DLAf-1hOnx6T247hfwG0hReM'
+BASE_URL = 'https://api.telegram.org/bot' + TOKEN + '/'
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, Nothing at this URL.', 404
+
+
+@app.errorhandler(500)
+def application_error(e):
+    """Return a custom 500 error."""
+    return 'Sorry, unexpected error: {}'.format(e), 500

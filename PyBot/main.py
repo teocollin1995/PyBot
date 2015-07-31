e1485eda6825a13784559904f7fe37fccde49157
teocollin1995 <teocollin1995@gmail.com>
2015-07-31 18:41:57 -0400

    erg 3

--- PyBot/main.py
+++ PyBot/main.py
@@ -9,12 +9,6 @@ import webapp2
 
 TOKEN= '50177117:AAGCMNPVi73DLAf-1hOnx6T247hfwG0hReM'
 BASE_URL = 'https://api.telegram.org/bot' + TOKEN + '/'
-import StringIO
-import json
-import logging
-import random
-import urllib
-import urllib2
 
 # ================================
 

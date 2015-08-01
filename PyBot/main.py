079ca76836c71cf01e9787e09ebf4882b406e111
teocollin1995 <teocollin1995@gmail.com>
2015-07-31 23:54:51 -0400

    more

--- PyBot/main.py
+++ PyBot/main.py
@@ -27,6 +27,14 @@ def wh():
     body = jsonify(json.loads(request.data))
     logging.info('request body:')
     logging.info(body)
+    update_id = body['update_id']
+    message = body['message']
+    message_id = message.get('message_id')
+    date = message.get('date')
+    text = message.get('text')
+    fr = message.get('from')
+    chat = message['chat']
+    chat_id = chat['id']
     resp = urllib2.urlopen(BASE_URL + 'sendMessage', urllib.urlencode({
         'chat_id': str(chat_id),
         'text': msg.encode('utf-8'),
